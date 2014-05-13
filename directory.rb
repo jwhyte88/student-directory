03078567b7b7c59ab0f9c6e07b017cbb2ac2fffc
Nic Yeeles <n.yeeles@gmail.com>
2014-05-13 15:20:09 +0100

    Started using methods.

--- directory.rb
+++ directory.rb
@@ -19,10 +19,21 @@ students = [
 "Nadav Matalon",
 "Fitsum Teklehaimanot"]
 
-puts "The students of my cohort at Makers Academy"
-puts "-------------"
-students.each do |student|
-  puts student
+def print_header
+	puts "The students of my cohort at Makers Academy"
+	puts "-------------"
 end
-# finally, we print the total
-puts "Overall, we have #{students.length} great students"
+
+def print(names)
+	names.each do |name|
+  	puts name
+  end
+end
+
+def print_footer(names)
+	puts "Overall, we have #{names.length} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
