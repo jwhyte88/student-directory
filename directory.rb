8e9a9a0a9b2c0599b0dc1439402df87ca49b2976
Nic Yeeles <n.yeeles@gmail.com>
2014-05-13 12:13:48 +0100

    Added directory.rb

---
+++ directory.rb
@@ -0,0 +1,48 @@
+puts "The students on my cohort of Makers Academy"
+puts "James Kemp"
+puts "Nic Yeeles"
+puts "Roi Driscoll"
+puts "James Whyte"
+puts "Margherita Serino"
+puts "Vaidas Mykolaitis"
+puts "Johann Bornman"
+puts "Kate Hamilton"
+puts "James Kemp"
+puts "Nic Yeeles"
+puts "Julie Walker"
+puts "Will Allen"
+puts "Julia Tan"
+puts "Federico Maffei"
+puts "Jamie Patel"
+puts "Faezrah Rizalman"
+puts "Josh Fail-Broon"
+puts "Sasha Cooper"
+puts "Nicolai DTH"
+puts "Nadav Matalon"
+puts "Fitsum Teklehaimanot"
+
+print "Overall, we have "
+print 9
+puts " great students"
+
+# students = ["Roi Driscoll",
+# "James Whyte",
+# "Margherita Serino",
+# "Vaidas Mykolaitis",
+# "Johann Bornman",
+# "Kate Hamilton",
+# "James Kemp",
+# "Nic Yeeles",
+# "Julie Walker",
+# "Will Allen",
+# "Julia Tan",
+# "Federico Maffei",
+# "Jamie Patel",
+# "Faezrah Rizalman",
+# "Josh Fail-Broon",
+# "Sasha Cooper",
+# "Nicolai DTH",
+# "Nadav Matalon",
+# "Fitsum Teklehaimanot"]
+
+# students.each { |name| puts "Hello #{name}" }
