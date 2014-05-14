f832ac26f61763558e2889ada1ceef341c61475f
Nic Yeeles <n.yeeles@gmail.com>
2014-05-14 10:54:21 +0100

    Added a hash instead of array

--- directory.rb
+++ directory.rb
@@ -1,32 +1,33 @@
 students = [
-"Roi Driscoll",
-"James Whyte",
-"Margherita Serino",
-"Vaidas Mykolaitis",
-"Johann Bornman",
-"Kate Hamilton",
-"James Kemp",
-"Nic Yeeles",
-"Julie Walker",
-"Will Allen",
-"Julia Tan",
-"Federico Maffei",
-"Jamie Patel",
-"Faezrah Rizalman",
-"Josh Fail-Broon",
-"Sasha Cooper",
-"Nicolai DTH",
-"Nadav Matalon",
-"Fitsum Teklehaimanot"]
+	{:name => "Roi Driscoll", :cohort => :may},
+	{:name => "James Whyte", :cohort => :may},
+	{:name => "Margherita Serino", :cohort => :may},
+	{:name => "Vaidas Mykolaitis", :cohort => :may},
+	{:name => "Johann Bornman", :cohort => :may},
+	{:name => "Kate Hamilton", :cohort => :may},
+	{:name => "James Kemp", :cohort => :may},
+	{:name => "Nic Yeeles", :cohort => :may},
+	{:name => "Julie Walker", :cohort => :may},
+	{:name => "Will Allen", :cohort => :may},
+	{:name => "Julia Tan", :cohort => :may},
+	{:name => "Federico Maffei", :cohort => :may},
+	{:name => "Jamie Patel", :cohort => :may},
+	{:name => "Faezrah Rizalman", :cohort => :may},
+	{:name => "Josh Fail-Broon", :cohort => :may},
+	{:name => "Sasha Cooper", :cohort => :may},
+	{:name => "Nicolai DTH", :cohort => :may},
+	{:name => "Nadav Matalon", :cohort => :may},
+	{:name => "Fitsum Teklehaimanot", :cohort => :may}
+]
 
-def print_header
+def print_first_sentence
 	puts "The students of my cohort at Makers Academy"
 	puts "-------------"
 end
 
-def print(names)
-	names.each do |name|
-  	puts name
+def print_student_names(students)
+	students.each do |student|
+  	puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
@@ -34,6 +35,6 @@ def print_footer(names)
 	puts "Overall, we have #{names.length} great students"
 end
 
-print_header
-print(students)
+print_first_sentence
+print_student_names(students)
 print_footer(students)
