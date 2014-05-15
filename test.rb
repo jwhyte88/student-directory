b1e0e073e6d033ad313d305867e1efc6324ce012
Nic Yeeles <n.yeeles@gmail.com>
2014-05-15 22:25:23 +0100

    Added save to csv file functionality

--- test.rb
+++ test.rb
@@ -1,16 +1,28 @@
-class1 = ["bob", "dave", "nic"]
-class2 = ["steve", "mike", "john"]
+# @students = { :name => "Bob", :cohort => :may, :name => "Joe", :cohort => :may, :name => "Mike", :cohort => :may, }
 
-
-def print_students_names(x)
-	counter = 0
-
-	until counter == x.count do
-		puts x[counter]
-		counter += 1
-	end
+def save_students
+  file = File.open("students.csv", "w")
+  
+    file.puts "Hello"
+  
+  file.close
 end
 
+save_students
 
-print_students_names(class1)
-print_students_names(class2)
+# class1 = ["bob", "dave", "nic"]
+# class2 = ["steve", "mike", "john"]
+
+
+# def print_students_names(x)
+# 	counter = 0
+
+# 	until counter == x.count do
+# 		puts x[counter]
+# 		counter += 1
+# 	end
+# end
+
+
+# print_students_names(class1)
+# print_students_names(class2)
